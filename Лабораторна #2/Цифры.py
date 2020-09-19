82299ef8970e3c46779913ec60083687dfb512d2
Pyrozhenko Ivan <71495576+pyrozhenko@users.noreply.github.com>
2020-09-19 10:02:31 +0300

    Add files via upload

---
+++ Лабораторна #2/Цифры.py
@@ -0,0 +1,9 @@
+Python 3.8.6rc1 (tags/v3.8.6rc1:08bd63d, Sep  7 2020, 23:10:23) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print (3 + 4)
+7
+>>> print (3 * 5)
+15
+>>> print ( 4**2)
+16
+>>> 
